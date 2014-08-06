f09d3e6bd92abdacc6fb313c59455ce3b1b23914
ddlarosa <ddlarosa@ddlarosa-ProLiant-ML110-G4.(none)>
2014-08-06 11:27:51 +0200

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Xmlinvoce::Application.config.secret_key_base = '8d4957fc582354e0561afb9df5deb332a1ab54ea929a395b5da785411666f94bcf284e30939361bb0649fffca1e127b5a6913be7c8bdf424425c1630d2145f83'
